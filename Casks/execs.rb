a82ca90639b8227595c8ddebd127e47e0fc13208
Daeho Ro <lamanus@outlook.kr>
2024-04-04 17:35:40 +0900

    execs 0.1.8

--- Casks/execs.rb
+++ Casks/execs.rb
@@ -1,9 +1,9 @@
 cask "execs" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.1.7"
-  sha256 arm:   "0a6cb35fc025718881a716838246e631cd55687f950f251709a2c10100c0181b",
-         intel: "0169055cf01d5eca887d596237360bbe4518b997d8114f2d41083ff4db65e830"
+  version "0.1.8"
+  sha256 arm:   "5628091f679a1d3e139428cf2c67e3f600dd84edc7f3c79f5e5f48b981872303",
+         intel: "72b328bbc4a6184fe5e483fde4952801e6cbcf6daaba2c3a8e49c513a3bdffcc"
 
   url "https://github.com/daeho-ro/execs/releases/download/v#{version}/execs_#{version}_darwin_#{arch}"
   name "execs"

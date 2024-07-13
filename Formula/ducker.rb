1f610378c74273ccf5a7f047cac5fb7f31cfedb7
Daeho Ro <lamanus@outlook.kr>
2024-07-13 22:23:10 +0900

    ducker 0.0.6

--- Formula/ducker.rb
+++ Formula/ducker.rb
@@ -1,8 +1,8 @@
 class Ducker < Formula
   desc "Slightly quackers Docker TUI based on k9s"
   homepage "https://github.com/robertpsoane/ducker"
-  url "https://github.com/robertpsoane/ducker/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "54b23d515e219d14b6f7f02c47889ba2696084594db30637f13afcfe89eb5824"
+  url "https://github.com/robertpsoane/ducker/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "4e90fca874d295a4c52e2e9c434a36de809ceedcbef4db428858092393f7eb07"
   license "MIT"
   head "https://github.com/robertpsoane/ducker.git", branch: "master"
 

c18ec5c0b9bfc9d8c58bad10d90ed73d3fcd5ae3
Daeho Ro <lamanus@outlook.kr>
2024-07-17 21:53:26 +0900

    notepadnext 0.8

--- Casks/notepadnext.rb
+++ Casks/notepadnext.rb
@@ -1,6 +1,6 @@
 cask "notepadnext" do
-  version "0.7"
-  sha256 "50db724e698ec0756fbbf4f5cd17aaed48d9b089a333a9820603729da32ff166"
+  version "0.8"
+  sha256 "544c64b6f04c6efb33ef3b7d375ca39fcfafa10dd65c9439e13af6d16181d590"
 
   url "https://github.com/dail8859/NotepadNext/releases/download/v#{version}/NotepadNext-v#{version}.dmg"
   name "notepad-next"

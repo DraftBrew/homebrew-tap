b9fc96fa3a1757f5316a9a63ada249a168a02421
Daeho Ro <lamanus@outlook.kr>
2025-02-24 21:45:06 +0900

    boring-notch 2.6

--- Casks/boring-notch.rb
+++ Casks/boring-notch.rb
@@ -1,8 +1,8 @@
 cask "boring-notch" do
-  version "2.5,jellyfin.snoring"
-  sha256 "97780877acdbecc6701fc5970478c708f36417b3c28c2fda3db07732645fdce0"
+  version "2.6,wolf.painting"
+  sha256 "1a58ec27e5de30faf107fdf8b77575b1c39ace69e77b1330fc4ed6562bf2badc"
 
-  url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version.csv.second}/JellyfinSnoring.dmg",
+  url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version.csv.second}/WolfPainting.dmg",
       verified: "github.com/TheBoredTeam/boring.notch/"
   name "TheBoringNotch"
   desc "Transform Space: Not so boring Notch"

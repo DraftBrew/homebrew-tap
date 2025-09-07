715e0aaea48c5d4d1f4fb9402edfbd35f1c7f589
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-09-07 21:10:59 +0900

    pomocore 1.2.51

--- Casks/pomocore.rb
+++ Casks/pomocore.rb
@@ -1,6 +1,6 @@
 cask "pomocore" do
-  version "1.1.6"
-  sha256 "52461019bae6414d4af97f1e7a172b2231815dcc295452a61ba628b1341fcb55"
+  version "1.2.51"
+  sha256 "98dce7b134bde8bbcea1557e76fac5d61d87d70080122af8865fd955af3b0968"
 
   url "https://github.com/swmaeStrong/Pawcus-Public/releases/download/v#{version}/Pomocore.dmg",
       verified: "github.com/swmaeStrong/Pawcus-Public/"

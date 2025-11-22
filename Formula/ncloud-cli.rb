76c10d13799e7c07252b47fd9015b661e2cdd812
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-11-22 22:53:49 +0900

    ncloud-cli 1.1.27-20251120

--- Formula/ncloud-cli.rb
+++ Formula/ncloud-cli.rb
@@ -1,14 +1,17 @@
 class NcloudCli < Formula
   desc "Command-line interface for Naver Cloud Platform"
   homepage "https://cli.ncloud-docs.com/docs/guide"
-  url "https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.25_20250717.zip"
-  version "1.1.25"
-  sha256 "be98ac0d20382c3563c02c9776dea16d6c4c1bbb4f8f3927359602d5c4f033ba"
+  url "https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.27_20251120.zip"
+  version "1.1.27-20251120"
+  sha256 "87043a13e081c0f63d47a2aba6d351985b6497cced637bca09ff35041212638f"
   license :cannot_represent
 
   livecheck do
     url "https://cli.ncloud-docs.com/docs/guide-clichange"
-    regex(/CLI[._-]v?(\d+(?:\.\d+)+)[._-]\d+.zip/i)
+    regex(/CLI[._-]v?(\d+(?:\.\d+)+)[._-](\d{8}).zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]}-#{match[1]}" }
+    end
   end
 
   depends_on "openjdk"

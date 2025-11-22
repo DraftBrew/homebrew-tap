12e8c967d3f6f15eaf4afb88aefdd145159b0e9a
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-11-22 23:01:46 +0900

    co-molib: renamed from pomocore

--- Casks/co-molib.rb
+++ Casks/co-molib.rb
@@ -1,16 +1,16 @@
-cask "pomocore" do
-  version "1.2.51"
-  sha256 "98dce7b134bde8bbcea1557e76fac5d61d87d70080122af8865fd955af3b0968"
+cask "co-molib" do
+  version "1.2.84"
+  sha256 "3215bfe656d4b484e71d89aafbbb5b15b9744eec78f0bb62dea777f6d835eb59"
 
-  url "https://github.com/swmaeStrong/Pawcus-Public/releases/download/v#{version}/Pomocore.dmg",
+  url "https://github.com/swmaeStrong/Pawcus-Public/releases/download/v#{version}/Co-molib.dmg",
       verified: "github.com/swmaeStrong/Pawcus-Public/"
-  name "Pomocore"
+  name "Co-molib"
   desc "Developer productivity timer and app usage tracker"
-  homepage "https://www.pomocore.com/"
+  homepage "https://www.co-molib.com/"
 
   depends_on macos: ">= :sonoma"
 
-  app "Pomocore.app"
+  app "Comolib.app"
 
   zap trash: [
     "~/Library/Application Support/Pawcus",

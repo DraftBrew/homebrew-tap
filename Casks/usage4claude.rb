d0b870e8169b1d176bf707a93b00dbda28ef8a6d
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-18 15:49:42 +0900

    usage4claude 1.6.0 (new cask)

---
+++ Casks/usage4claude.rb
@@ -0,0 +1,25 @@
+cask "usage4claude" do
+  version "1.6.0"
+  sha256 "87c84700248379e08adf47ebd62fc5587d4d3f7cacd00d9b4adbd72f36133380"
+
+  url "https://github.com/f-is-h/Usage4Claude/releases/download/v#{version}/Usage4Claude-v#{version}.dmg"
+  name "Usage4Claude"
+  desc "Monitor your Claude AI 5-hour/7-day usage quota in real-time on the menubar"
+  homepage "https://github.com/f-is-h/Usage4Claude"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Usage4Claude.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/xyz.fi5h.Usage4Claude",
+    "~/Library/Containers/xyz.fi5h.Usage4Claude",
+  ]
+
+  caveats do
+    "`--no-quarantine` option is required to install"
+  end
+end
